7ca6f6b0e8eee5dd456ab3267b0f44d3368cb066
jeremymsimon <37712091+jeremymsimon@users.noreply.github.com>
2023-03-09 10:42:49 +1100

    Update main script with options parsed

--- FacultyCoPubSearchNetworks.R
+++ FacultyCoPubSearchNetworks.R
@@ -1,22 +1,58 @@
-library(reutils)
-library(tidyverse)
-library(XML)
-library(readxl)
+suppressPackageStartupMessages(library(reutils))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(XML))
+suppressPackageStartupMessages(library(readxl))
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(tools))
+suppressPackageStartupMessages(library(qgraph))
+suppressPackageStartupMessages(library(RColorBrewer))
+suppressPackageStartupMessages(library(gplots))
 
-mindate <- 2013
-maxdate <- 2023
+# Parse input options
+option_list <- list( 
+    make_option(c("-i", "--input"), 
+	type="character", 
+        help="Filename of input table containing faculty names\n\t\tMust be tab-delimited text or xlsx\n\t\t\tColumn 1: AuthorLast AuthorFirst\n\t\t\tColumn 2: AuthorLast AuthorFirstInitial\n\t\t\tColumn 3: AuthorRole",
+        metavar="FILENAME"),
+    make_option("--minyear", 
+	type="double", 
+        help = "Beginning year of date range for query (YYYY)",
+        metavar="MINYEAR"),
+    make_option("--maxyear", 
+	type="double", 
+        help="End year of date range for query (YYYY)",
+        metavar="MAXYEAR"),
+    make_option(c("-a","--affl"), 
+	type="character",
+        help="Author affiliation for query, e.g. 'University of North Carolina'",
+        metavar="AFFL")
+    )
 
-input_list <- read_excel("NBIO_curriculum_faculty_list_2023-03-08.xlsx",col_names=c("Full","Short","Title"))
+opt <- parse_args(OptionParser(option_list=option_list))
 
+mindate <- opt$minyear
+maxdate <- opt$maxyear
+input <- opt$input
+affl <- opt$affl
+
+# Import author table
+if(file_ext(input)=="xlsx") {
+	input_list <- read_excel(input,col_names=c("Full","Short","Title"))
+} else{
+	input_list <- read_tsv(input,col_names=c("Full","Short","Title"))
+}
+
+
+# Perform pubmed queries
 results_full <- c()
 results_counts <- c()
 
 for(i in 1:length(input_list$Full)) {
 	for(j in 1:length(input_list$Full)) {
 		if(i==j) next
-	   	# Incorporate sleeps to slow down queries and avoid error about too many requests
-	   	if(i %% 2 == 0 || j %% 2 == 0) {
-			Sys.sleep(10)
+		# Incorporate sleeps to slow down queries and avoid error about too many requests
+		if(i %% 2 == 0 || j %% 2 == 0) {
+			Sys.sleep(3)
 		}
 		
 		auth1_full <- as.character(input_list[i,"Full"])
@@ -24,7 +60,7 @@ for(i in 1:length(input_list$Full)) {
 		auth1_short <- as.character(input_list[i,"Short"])
 		auth2_short <- as.character(input_list[j,"Short"])
 
-		term <- paste0("(",auth1_full, " [AUTH] University of North Carolina [AFFL] OR ",auth1_short," [AUTH] University of North Carolina [AFFL]) AND (",auth2_full, " [AUTH] University of North Carolina [AFFL] OR ",auth2_short," [AUTH] University of North Carolina [AFFL])")
+		term <- paste0("(",auth1_full, " [AUTH] ",affl," [AFFL] OR ",auth1_short," [AUTH] ",affl," [AFFL]) AND (",auth2_full, " [AUTH] ",affl," [AFFL] OR ",auth2_short," [AUTH] ",affl, " [AFFL])")
 		print(term)
 		e <- suppressMessages(esearch(db = "pubmed",
 					term = term,
@@ -63,5 +99,29 @@ for(i in 1:length(input_list$Full)) {
 	}	
 }
 
-write_tsv(results_full,"NBIO_curriculum_faculty_list_2023-03-08_coPublication_table_full.txt")
-write_tsv(results_counts,"NBIO_curriculum_faculty_list_2023-03-08_coPublication_table_counts.txt")
+# Write results
+write_tsv(results_full,"CoPublication_table_full.txt")
+write_tsv(results_counts,"CoPublication_table_counts.txt")
+
+# Generate network plot
+all_authors <- unique(c(results_counts$Author1,results_counts$Author2))
+
+mat <- matrix(data=0,nrow=length(all_authors),ncol=length(all_authors))
+rownames(mat) <- all_authors
+colnames(mat) <- all_authors
+
+for(i in 1:length(rownames(results_counts))) {
+	mat[which(rownames(mat)==as.character(results_counts[i,1])),which(colnames(mat)==as.character(results_counts[i,2]))] = as.numeric(results_counts[i,3])
+}
+
+pdf("CoPublication_network.pdf",width=20,height=20)
+qgraph(mat,
+	layout="spring",
+	directed=F,
+	labels=rownames(mat),
+	edge.color="black",
+	curve=0,
+	label.cex=3,
+	label.scale.equal=T)
+dev.off()
+
